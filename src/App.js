73a862edfa8c076dd220561810cb55b5716b1511
Syamlal <syamjayaraj@gmail.com>
2021-12-01 17:11:50 +0530

    updated to React hooks

--- src/App.js
+++ src/App.js
@@ -1,69 +1,59 @@
-import React, { Component } from "react";
-import "./App.css";
+import React, { useState, useEffect } from "react";
 
-export default class App extends Component {
-  constructor(props) {
-    super();
-    this.state = {
-      list: true,
-      card: false,
-      players: [],
-      player: {}
-    };
-  }
-  componentDidMount() {
+export default function App() {
+  const [list, setList] = useState(true);
+  const [card, setCard] = useState(false);
+  const [players, setPlayers] = useState([]);
+  const [player, setPlayer] = useState({});
+
+  useEffect(() => {
     fetch("http://localhost:3001/players/list")
-      .then(response => response.json())
-      .then(responseJson => {
-        this.setState({ players: responseJson.data });
+      .then((response) => response.json())
+      .then((responseJson) => {
+        setPlayers(responseJson.data);
       });
-  }
-  showCard = id => {
+  }, []);
+
+  let showCard = (id) => {
     fetch(`http://localhost:3001/players/${id}`)
-      .then(response => response.json())
-      .then(responseJson => {
-        this.setState({ player: responseJson.data });
+      .then((response) => response.json())
+      .then((responseJson) => {
+        setPlayer(responseJson.data);
+        setList(false);
+        setCard(true);
       });
-    this.setState({
-      list: false,
-      card: true
-    });
   };
 
-  showList = () => {
-    this.setState({
-      card: false,
-      list: true
-    });
+  let showList = () => {
+    setCard(false);
+    setList(true);
   };
 
-  render() {
-    return (
-      <div className="container">
-        {this.state.list ? (
-          <div className="list-group">
-            {this.state.players.map(player => (
-              <li
-                onClick={() => this.showCard(player._id)}
-                className="list-group-item list-group-item-action"
-              >
-                {player.name}
-              </li>
-            ))}
-          </div>
-        ) : null}
-        {this.state.card ? (
-          <div class="card" style={{ width: "18rem" }}>
-            <div class="card-body">
-              <h5 class="card-title">{this.state.player.name}</h5>
-              <p class="card-text">{this.state.player.runs}</p>
-              <div onClick={() => this.showList()} class="btn btn-primary">
-                Back
-              </div>
+  return (
+    <div className="container">
+      {list ? (
+        <div className="list-group">
+          {players.map((player) => (
+            <li
+              onClick={() => showCard(player._id)}
+              className="list-group-item list-group-item-action"
+            >
+              {player.name}
+            </li>
+          ))}
+        </div>
+      ) : null}
+      {card ? (
+        <div class="card" style={{ width: "18rem" }}>
+          <div class="card-body">
+            <h5 class="card-title">{player.name}</h5>
+            <p class="card-text">{player.runs}</p>
+            <div onClick={() => showList()} class="btn btn-primary">
+              Back
             </div>
           </div>
-        ) : null}
-      </div>
-    );
-  }
+        </div>
+      ) : null}
+    </div>
+  );
 }
